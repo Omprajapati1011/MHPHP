7c29fac2692ac0372d5ba26c381b162c84da1122
Om Prajapati <109795803+Omprajapati1011@users.noreply.github.com>
2024-11-15 10:53:23 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -6,7 +6,7 @@ require 'PHPMAILER\Exception.php'; // Include PHPMailer via Composer
 require 'PHPMAILER\PHPMailer.php'; 
 require 'PHPMAILER\SMTP.php'; 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // Get form input and sanitize
     $companyName = htmlspecialchars($_POST['company-name'] ?? '', ENT_QUOTES, 'UTF-8');
     $email = filter_var($_POST['email'] ?? '', FILTER_VALIDATE_EMAIL);
